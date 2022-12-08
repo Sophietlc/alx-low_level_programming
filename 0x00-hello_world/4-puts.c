718706a4891220997a62223cdcb47c26c8e7869c
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-08 13:03:37 -0800

    revised

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	int puts("\"Your programming is like building a multilingual puzzle"\n");
+	int puts("\"Your programming is like building a multilingual puzzle\n");
 	return o;
 }
