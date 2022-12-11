86137f40baa4606d1a0762f6ef04ddb40d2a9d9a
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-11 13:28:10 -0800

    new

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints all the alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	char new;
+
+	char sh;
+
+	new = '\n';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+
+	for (sh = 'A'; sh <= 'z'; sh++)
+	{
+		putchar(sh);
+	}
+
+	putchar(new);
+
+	return (0);
+}
