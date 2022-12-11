cfee19d28734e8909c364668427b5f842a7555bd
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-11 13:52:40 -0800

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints all the alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	char new;
+
+	new = '\n';
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar(new);
+
+	return (0);
+}
