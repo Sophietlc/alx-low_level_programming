84af89dddaf97c0e0e0fbcd804a13295e14af4f5
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-12 13:58:53 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ *main - Entry point
+ *
+ *Description: Program outputs a string of characters
+ *
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char str[] = "\" Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
