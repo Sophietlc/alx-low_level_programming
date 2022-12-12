ca0d630806c068055c5cdbd907b4d134c1bf4625
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-12 05:46:25 -0800

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints all the numbers of base16 in lowercase
+ *
+ * Return: Always 0 (Success
+ */
+
+int main(void)
+{
+	int num;
+
+	char az;
+
+	num = 0;
+
+	az = 'a';
+
+	while (num <= 9; num++)
+	{
+		putchar(num);
+	}
+
+	while (az <= 'f'; az++)
+	{
+		putchar(az);
+	}
+
+	putchar('\n')
+
+	return (0);
+}
