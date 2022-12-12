7651165b00d3b1cc1ef305d0d92f307ef0c53b00
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-12 06:06:46 -0800

    revised

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 0; ch < 10; ch++)
+	for (ch = '0'; ch < '10'; ch++)
 	{
 		putchar(ch);
 	}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : Program that prints 0-9 separated by commas
+ *
+ * Return: Always 0 (Success
+ */
+
+int main(void)
+{
+	int a;
+
+	a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+
+		if (a != '9')
+		{
+			putchar(',');
+
+			putchar(' ');
+		}
+		a++;
+	}
+	return (0);
+}
+
