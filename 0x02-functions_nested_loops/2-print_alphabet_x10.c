94c93df1d27f525e9e5355a83fa11f173a3a373b
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-14 13:27:36 -0800

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints an alphabet in lower case 10x
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void);
+{
+	char al;
+
+	int i;
+
+	i = 0;
+	al = 'a';
+	while (i < 10)
+	{
+		for (al <= 'z'; a++)
+		{
+			_putchar(al);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
