9804addcc2420f82976c00bf536e4df946823b93
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-15 14:37:31 -0800

    new

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns last digit of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+
+	if (x < 0)
+	{
+		x *= -1;
+	}
+	return (x);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,5 +15,6 @@ int print_sign(int n);
 
 int _abs(int n);
 
+int print_last_digit(int n);
 
 #endif
