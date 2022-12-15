5f0bf914e8897e6efca99bea3d6d5f544d54dcb1
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-15 14:11:01 -0800

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - checks for a number's absolute value
+ *
+ * @n: the number to be checked
+ *
+ * Return: 1 if >0, 0 if ==0, -1 if <0
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (-n);
+	}
+	else if (n < 0)
+	{
+		return (n);
+	}
+}
