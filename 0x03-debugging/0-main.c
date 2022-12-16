37c56e41fdd81201b4a1ac11fafd4f6c55a1f11a
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-16 11:45:37 -0800

    done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * main - tests function that prints if integer is positive or negative
  *
@@ -16,5 +14,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }
