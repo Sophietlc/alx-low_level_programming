e33996b83ccdb2ab0f731040a0a5385401b34b12
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-18 12:22:44 -0800

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0-9 followed by a newline
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
