ff3733a674d86842aba7502a0b8b2a4c02ef68bb
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-19 15:29:42 -0800

    done

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,7 +17,12 @@ void print_array(int *a, int n)
 	i = 0;
 	for (; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+
+		if (i != (n-1))
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
