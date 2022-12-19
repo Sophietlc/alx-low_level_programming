d9ad4bd8c181c158be110aca894d5fb960cb1ee7
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-19 15:13:35 -0800

    new

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array
+ *
+ * @a: the array
+ *
+ * @n: the number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (i <= n; i++)
+	{
+		printf("%d", a[i]);
+	}
+	printf('\n');
+}
