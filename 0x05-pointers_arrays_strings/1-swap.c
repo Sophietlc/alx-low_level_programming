ead04ba8f38d83f28cd84827d28fc95ef0ad53b7
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-19 11:53:58 -0800

    new

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a: integer to be swapped
+ *
+ * @b: integer to be swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
