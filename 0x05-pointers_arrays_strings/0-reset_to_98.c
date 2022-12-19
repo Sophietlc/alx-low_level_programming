a62932fc2e460ff97f2049652090ad83fd0fef05
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-19 11:40:29 -0800

    reduced word count

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
+ *reset_to_98 - takes a pointer and updates the value it points to to 98
+ *
+ * @n: pointer to the integer
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98
+	*n = 98;
 }
