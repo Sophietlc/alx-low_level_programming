b3962b6a8c79bdd1442fecb49577205a38f9bcaf
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-19 12:21:48 -0800

    new

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(str);
+	}
+	_putchar('\n')
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,8 @@ void swap_int(int *a, int *b);
 
 int _strlen(char *s);
 
+void _puts(char *str);
+
+int _putchar(char c);
+
 #endif
