5686f57f33b1e01080e91ffcaefe2ea1c073171c
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-20 12:59:49 -0800

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints the '-' character
+ *
+ * @n: the number of times - is printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar("-");
+		}
+	}
+	_putchar("\n");
+}
