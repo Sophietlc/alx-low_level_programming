5b99333af9cf89f91d8ca14097f8d7f4cbfee31d
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-20 14:14:24 -0800

    new

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int i;
+
+
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		j = 0;
+
+		for (i = 0; i < j, i++)
+		{
+			_putchar('\n');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
