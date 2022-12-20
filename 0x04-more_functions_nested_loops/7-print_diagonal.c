f1a2ac71a069f3dea378e54c64baf9ede59087a3
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-20 13:26:39 -0800

    added if condition

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,17 @@ void print_diagonal(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		_putchar('\\');
-
-		if ( i != (n -1))
+		for (i = 0; i < n; i++)
 		{
-			_putchar('\n');
-			_putchar(' ');
+			_putchar('\\');
+
+			if ( i != (n -1))
+			{
+				_putchar('\n');
+				_putchar(' ');
+			}
 		}
 	}
 	_putchar('\n');
