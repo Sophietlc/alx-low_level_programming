7cb900eff303d62669f20a9a5d88650696996b0a
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-20 13:58:38 -0800

    corrected

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,21 @@ void print_diagonal(int n)
 {
 	int i;
 
+
+	int j;
+
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar('\\');
-
-			if ( i != (n -1))
+			for (j = 0; j < i; j++)
 			{
-				_putchar('\n');
 				_putchar(' ');
 			}
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
