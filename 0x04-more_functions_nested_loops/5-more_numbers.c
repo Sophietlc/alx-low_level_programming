e6c69e6b29a9f6931ffbac9b94ee33ae4c79689e
Fanodun Sophie <sophiefanodun@gmail.com>
2022-12-20 12:45:51 -0800

    new

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	int x;
+
+	i = 0;
+	while (i <= 9)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (x > 9)
+			{
+				_putchar((x / 10) + '0');
+			}
+
+			_putchar((x % 10) + '0');
+		}
+		i++;
+	}
+}
