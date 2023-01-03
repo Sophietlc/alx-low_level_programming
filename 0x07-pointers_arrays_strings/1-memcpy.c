00eb41f9733721f011658ec4f21b8a70f6a07b3b
Sophietlc <sophiefanodun@gmail.com>
2023-01-03 13:31:43 -0800

    added a ;

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,13 +14,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	i = 0;
-
-	for (i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	return (dest)
+	return (dest);
 }
