fcf2facabb04032d187d5a812882dfe5af9a115c
Sophietlc <sophiefanodun@gmail.com>
2023-01-03 14:26:15 -0800

    updated

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,25 +18,26 @@ char *_strstr(char *haystack, char *needle)
 
 	while (haystack[i] != '\0')
 	{
-		if haystack[i] == needle[0]
+		if (haystack[i] == needle[0])
 		{
 			k = i;
 			j = 0;
 
-		while (needle[j] != '\0')
-		{
-			if (haystack[k] == needle[i])
+			while (needle[j] != '\0')
 			{
-				k++;
-				j++;
-			}
+				if (haystack[k] == needle[i])
+				{
+					k++;
+					j++;
+				}
 
-			else
-				break;
-		}
-		if (needle[j] == '\0')
-		{
-			return (haystack + i);
+				else
+					break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
 		}
 		i++;
 	}
