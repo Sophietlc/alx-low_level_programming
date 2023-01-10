f5a630f15d1ac764374c3342138f2346b33ae143
Sophietlc <sophiefanodun@gmail.com>
2023-01-10 14:04:04 -0800

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,10 +19,10 @@ char *create_array(unsigned int size, char c)
 
 	i = 0;
 
+	array = malloc(sizeof(char) * size);
+
 	if (i == 0 || array == NULL)
 		return (0);
-
-	array = malloc(sizeof(char) * size);
 
 	while (i < size)
 	{
