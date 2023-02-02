c740d212dac0aff3cb30c2047d4f5a9e0d9df0dd
Sophietlc <sophiefanodun@gmail.com>
2023-02-02 00:32:12 -0800

    edited

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -49,7 +49,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++) 
 		p[i] = s1[i];
 	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
+		p[i + j] = s2[j];
 	p[i + j] = '\0';
 
 	return (p);
