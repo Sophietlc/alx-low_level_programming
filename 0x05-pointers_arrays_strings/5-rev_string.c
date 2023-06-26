d0d087ca03885b2729ea19b30027b48d38cba872
Sophietlc <sophiefanodun@gmail.com>
2023-06-26 12:48:59 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	int len;
+	while(*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	int x = 0;
+	int y = len - 1;
+	char z;
+
+	while(x < y)
+	{
+		z = s[x];
+		s[x] = s[y];
+		s[y] = z;
+		x++, y--;
+	}
+	
+}
