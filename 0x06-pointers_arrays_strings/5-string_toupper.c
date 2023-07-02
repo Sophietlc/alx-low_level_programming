193ccedaf90a6702290ef256257c463df46d831e
Sophietlc <sophiefanodun@gmail.com>
2023-07-02 06:54:58 -0700

    new commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - a function that changes all lowercase letters 
+ * of a string to uppercase
+ * @c: the character
+ * Return: uppercase string
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (int i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z';)
+			c[i] += 32;
+	}
+	return (c);
+}
